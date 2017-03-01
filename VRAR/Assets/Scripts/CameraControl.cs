e49f0432363f049fd93515f67d8c3f360d40af02
ChenChen <chenchen.charliechen@gmail.com>
2017-03-01 12:14:06 -0800

    Uploading Subsystem(GPS) and Unity3D Project

---
+++ VRAR/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class CameraControl : MonoBehaviour {
+    #region GPS Variables
+    public string DEVICE;
+    public long TIME_STAMP;
+    public bool STATUS;
+    public float LAT;
+    public float LON;
+    public float ALT;
+    public int SAT;
+    public int PREC;
+    public long CHARS;
+    public int SENTENCES;
+    public int CSUM_ERR;
+    public float iniLAT;
+    public float iniLON;
+    public float iniALT;
+
+    public bool firstGPSflag;
+    public static string COMPort = "COM3";
+    public static int BAUDRate = 115200;
+
+    SerialPort streamCable = new SerialPort(COMPort, BAUDRate); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+    #endregion
+
+    #region Player Control Variables
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public int playerControlSpeed = 10;
+    public bool playerControlFlag;
+    #endregion
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Start  ");
+        firstGPSflag = true;
+        playerControlFlag = true;
+        streamCable.Open(); //Open the Serial Stream.
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!playerControlFlag) {
+            readGPS();
+            move();
+            if (Input.GetKeyDown(KeyCode.C)) {
+                playerControlFlag = true;
+                LAT = 0;
+                LON = 0;
+                iniLAT = 0;
+                iniLON = 0;
+            }
+        } else {
+            // ccmove();
+            playerControl();
+            if (Input.GetKeyDown(KeyCode.C)) {
+                playerControlFlag = false;
+            }
+        }
+    }
+
+    void readGPS() {
+        if (streamCable.IsOpen) {
+            string jsonString = streamCable.ReadLine();
+            print(jsonString);
+            try {
+                JsonUtility.FromJsonOverwrite(jsonString, this);
+                Debug.Log("device = " + this.DEVICE);
+                Debug.Log("time_stamp = " + this.TIME_STAMP);
+                Debug.Log("status = " + this.STATUS);
+                Debug.Log("lat = " + this.LAT);
+                Debug.Log("lon = " + this.LON);
+                Debug.Log("alt = " + this.ALT);
+                Debug.Log("sat = " + this.SAT);
+                Debug.Log("prec = " + this.PREC);
+                Debug.Log("chars = " + this.CHARS);
+                Debug.Log("sentences = " + this.SENTENCES);
+                Debug.Log("csum_err = " + this.CSUM_ERR);
+            } catch (System.Exception) {
+                Debug.Log("fail to Json.");
+                throw;
+            }
+        } else {
+            Debug.Log("No  ");
+        }
+    }
+
+    void ccmove() {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void playerControl() {
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(new Vector3(playerControlSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(new Vector3(-playerControlSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.Translate(new Vector3(0, -playerControlSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.E)) {
+            transform.Translate(new Vector3(0, playerControlSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(new Vector3(0, 0, playerControlSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(new Vector3(0, 0, -playerControlSpeed * Time.deltaTime));
+        }
+    }
+
+    void move() {
+        if (firstGPSflag == true && LON != 0 && LAT != 0 && ALT != 0) {
+            iniLON = LON;
+            iniLAT = LAT;
+            iniALT = ALT;
+            firstGPSflag = false;
+        } else if (LON != 0 && LAT != 0) {
+            float la = (float)((LAT - 53.178469) / 0.00001 * 0.12179047095976932582726898256213);
+            float lo = (float)((LON - 53.178469) / 0.00001 * 0.12179047095976932582726898256213);
+            float ila = (float)((iniLAT - 53.178469) / 0.00001 * 0.12179047095976932582726898256213);
+            float ilo = (float)((iniLON - 53.178469) / 0.00001 * 0.12179047095976932582726898256213);
+            //transform.position = Quaternion.AngleAxis(LON-iniLON, -Vector3.up) * Quaternion.AngleAxis(LAT-iniLAT, -Vector3.right) * new Vector3(0, 0, 1);
+            transform.position = new Vector3((lo - ilo), 0.5f, (la - ila));
+        }
+    }
+}
---
+++ VRAR/Assets/Scripts/GenerateAndChangeLandscape.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType { none, grass, sand, snow, cloud, maxBlock };
+
+public class Block {
+    public BlockType _type;
+    public bool _vis;
+    public GameObject _block;
+
+    public Block(BlockType type, bool vis, GameObject block) {
+        _type = type;
+        _vis = vis;
+        _block = block;
+    }
+}
+
+public class GenerateAndChangeLandscape : MonoBehaviour {
+    // x
+    public static int _width = 150;
+    // y
+    public static int _height = 150 + 100;
+    // z
+    public static int _depth = 150;
+
+    public static Camera main;
+
+    // PerlinNoise Parameters
+    public int _heightScale = 20;
+    public float _detailScale = 25.0f;
+    public int _heightOffset = 100;
+
+    // GameObjects
+    public GameObject _grassBlock;
+    public GameObject _sandBlock;
+    public GameObject _snowBlock;
+    public GameObject _cloudBlock;
+
+    Block[,,] worldBlocks = new Block[_width, _height, _depth];
+
+    // Use this for initialization
+    void Start() {
+        int seed = (int)Network.time * 10;
+        for (int z = 0; z < _depth; z++) {
+            for (int x = 0; x < _width; x++) {
+                // If you want just a flat plane
+                // int y = 0;
+                // If you want regular randomness:
+                // int y = (int)Random.Range(0, 10);
+                // If you want smooth randomness:
+                int y = (int)(Mathf.PerlinNoise((x + seed) / _detailScale, (z + seed) / _detailScale) * _heightScale) + _heightOffset;
+                Vector3 blockPos = new Vector3(x, y, z);
+
+                CreateBlock(y, blockPos, true);
+                while (y > 0) {
+                    y--;
+                    // blockPos = new Vector3(x, y, z);
+                    blockPos.y = y;
+                    CreateBlock(y, blockPos, false);
+                }
+            }
+        }
+
+        CreateCloud(20, 100);
+    }
+
+    void CreateBlock(int y, Vector3 blockPos, bool create) {
+        if (blockPos.x < 0 || blockPos.x >= _width || blockPos.y < 0 || blockPos.y >= _height || blockPos.z < 0 || blockPos.x >= _depth) return;
+        GameObject newBlock = null;
+
+        if (y > 15 + _heightOffset) {
+            if (create)
+                newBlock = (GameObject)Instantiate(_snowBlock, blockPos, Quaternion.identity);
+            worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = new Block(BlockType.snow, create, newBlock);
+        } else if (y > 5 + _heightOffset) {
+            if (create)
+                newBlock = (GameObject)Instantiate(_grassBlock, blockPos, Quaternion.identity);
+            worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = new Block(BlockType.grass, create, newBlock);
+        } else {
+            if (create)
+                newBlock = (GameObject)Instantiate(_sandBlock, blockPos, Quaternion.identity);
+            worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = new Block(BlockType.sand, create, newBlock);
+        }
+    }
+
+    void CreateCloud(int numClouds, int cSize) {
+        for (int i = 0; i < numClouds; i++) {
+            int xpos = Random.Range(0, _width - 1);
+            int zpos = Random.Range(0, _depth - 1);
+            for (int j = 0; j < cSize; j++) {
+                Vector3 blockPos = new Vector3(xpos, _height - 1, zpos);
+                GameObject newBlock = (GameObject)Instantiate(_cloudBlock, blockPos, Quaternion.identity);
+                // Debug.Log(blockPos);
+                worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = new Block(BlockType.cloud, true, newBlock);
+                xpos += Random.Range(-1, 2);
+                zpos += Random.Range(-1, 2);
+                if (xpos < 0 || xpos >= _width) xpos = _width / 2;
+                if (zpos < 0 || zpos >= _depth) zpos = _depth / 2;
+            }
+        }
+    }
+
+    void DrawBlock(Vector3 blockPos) {
+        //
+        // Prevent drawing outside of the permitted space
+        //
+        if (blockPos.x < 0 || blockPos.x >= _width || blockPos.y < 0 || blockPos.y >= _height || blockPos.z < 0 || blockPos.z >= _depth) return;
+
+        if (worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] == null) return;
+
+        if (!worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._vis) {
+            GameObject newBlock = null;
+            worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._vis = true;
+            if (worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._type == BlockType.grass) {
+                newBlock = (GameObject)Instantiate(_grassBlock, blockPos, Quaternion.identity);
+            } else if (worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._type == BlockType.sand) {
+                newBlock = (GameObject)Instantiate(_sandBlock, blockPos, Quaternion.identity);
+            } else if (worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._type == BlockType.snow) {
+                newBlock = (GameObject)Instantiate(_snowBlock, blockPos, Quaternion.identity);
+            } else {
+                worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._vis = false;
+            }
+
+            if (newBlock != null)
+                worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z]._block = newBlock;
+        }
+    }
+
+    int NeighbourCount(Vector3 blockPos) {
+        int nCount = 0;
+        for (int x = -1; x <= 1; x++)
+            for (int y = -1; y <= 1; y++)
+                for (int z = -1; z <= 1; z++) {
+                    if (!(x == 0 && y == 0 && z == 0)) {
+                        if (worldBlocks[(int)blockPos.x + x, (int)blockPos.y + y, (int)blockPos.z + z] != null)
+                            nCount++;
+                    }
+                }
+        return (nCount);
+    }
+
+    void CheckObscuredNeighbours(Vector3 blockPos) {
+        for (int x = -1; x <= 1; x++)
+            for (int y = -1; y <= 1; y++)
+                for (int z = -1; z <= 1; z++) {
+                    if (!(x == 0 && y == 0 && z == 0)) {
+                        Vector3 neighbour = new Vector3(blockPos.x + x, blockPos.y + y, blockPos.z + z);
+
+                        //if it is outside the map
+                        if (neighbour.x < 1 || neighbour.x > _width - 2 ||
+                            neighbour.y < 1 || neighbour.y > _height - 2 ||
+                            neighbour.z < 1 || neighbour.z > _depth - 2) continue;
+
+
+                        if (worldBlocks[(int)neighbour.x, (int)neighbour.y, (int)neighbour.z] != null) {
+                            if (NeighbourCount(neighbour) == 26) {
+                                Destroy(worldBlocks[(int)neighbour.x, (int)neighbour.y, (int)neighbour.z]._block);
+                                worldBlocks[(int)neighbour.x, (int)neighbour.y, (int)neighbour.z] = null;
+                            }
+                        }
+                    }
+                }
+    }
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            // Ray ray = Camera.allCameras[0].ScreenPointToRay(new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0));
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0));
+            // Debug.Log(ray);
+            if (Physics.Raycast(ray, out hit, 5000.0f)) {
+                Vector3 blockPos = hit.transform.position;
+
+                // this is the bottom block. Don't delete it!!!!
+                if ((int)blockPos.y == 0) return;
+
+                worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = null;
+
+                Destroy(hit.transform.gameObject);
+
+                //
+                // Show hidden structure underneath
+                // Only instantiate hidden blocks when they are on demand!
+                //
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = -1; y <= 1; y++) {
+                        for (int z = -1; z <= 1; z++) {
+                            if (!(x == 0 && y == 0 && z == 0)) {
+                                Vector3 neighbour = new Vector3(blockPos.x + x, blockPos.y + y, blockPos.z + z);
+                                DrawBlock(neighbour);
+                            }
+                        }
+                    }
+                }
+            }
+        } else if (Input.GetMouseButtonDown(1)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0));
+            // Debug.Log(ray.direction);
+            if (Physics.Raycast(ray, out hit, 5000.0f)) {
+                Vector3 blockPos = hit.transform.position;
+                Vector3 hitVector = blockPos - hit.point;
+
+                hitVector.x = Mathf.Abs(hitVector.x);
+                hitVector.y = Mathf.Abs(hitVector.y);
+                hitVector.z = Mathf.Abs(hitVector.z);
+
+                // Debug.Log(ray.direction);
+
+                if (hitVector.x > hitVector.z && hitVector.x > hitVector.y) {
+                    // blockPos.x -= (int)Mathf.RoundToInt(ray.direction.x);
+                    if (ray.direction.x > 0) {
+                        blockPos.x -= 1;
+                    } else {
+                        blockPos.x += 1;
+                    }
+                } else if (hitVector.y > hitVector.x && hitVector.y > hitVector.z) {
+                    // blockPos.y -= (int)Mathf.RoundToInt(ray.direction.y);
+                    if (ray.direction.y > 0) {
+                        blockPos.y -= 1;
+                    } else {
+                        blockPos.y += 1;
+                    }
+                } else {
+                    // blockPos.z -= (int)Mathf.RoundToInt(ray.direction.z);
+                    if (ray.direction.z > 0) {
+                        blockPos.z -= 1;
+                    } else {
+                        blockPos.z += 1;
+                    }
+                }
+
+
+                CreateBlock((int)blockPos.y, blockPos, true);
+                CheckObscuredNeighbours(blockPos);
+            }
+        }
+    }
+}
