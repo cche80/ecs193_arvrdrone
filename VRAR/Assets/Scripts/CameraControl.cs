64a7b0267482d95d935edb2656805fa9d67bbcf7
ChenChen <chenchen.charliechen@gmail.com>
2017-03-01 21:31:37 -0800

    GPS Code Added

--- VRAR/Assets/Scripts/CameraControl.cs
+++ VRAR/Assets/Scripts/CameraControl.cs
@@ -2,29 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using System.Text;
 
 public class CameraControl : MonoBehaviour {
-    #region GPS Variables
-    public string DEVICE;
-    public long TIME_STAMP;
-    public bool STATUS;
-    public float LAT;
-    public float LON;
-    public float ALT;
-    public int SAT;
-    public int PREC;
-    public long CHARS;
-    public int SENTENCES;
-    public int CSUM_ERR;
-    public float iniLAT;
-    public float iniLON;
-    public float iniALT;
+    public bool DEBUG = false;
 
-    public bool firstGPSflag;
-    public static string COMPort = "COM3";
-    public static int BAUDRate = 115200;
+    #region GPS and Serial Variables
+    private string DEVICE;
+    private long TIME_STAMP;
+    private bool STATUS;
+    private float LAT;
+    private float LON;
+    private float ALT;
+    private int SAT;
+    private int PREC;
+    private long CHARS;
+    private int SENTENCES;
+    private int CSUM_ERR;
+    private float iniLAT;
+    private float iniLON;
+    private float iniALT;
 
-    SerialPort streamCable = new SerialPort(COMPort, BAUDRate); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+    // Set the COM Port (COM4) and the BAUD Rate (9600 for XBee Connection).
+    private static string COMPort = "COM4";
+    private static int BAUDRate = 9600;
+    SerialPort streamCable = new SerialPort(COMPort, BAUDRate);
+
+    private bool readDoneFlg;
+    private bool firstGPSFlg;
+    StringBuilder jsonBuffer = new StringBuilder();
     #endregion
 
     #region Player Control Variables
@@ -34,24 +40,46 @@ public class CameraControl : MonoBehaviour {
     private Vector3 moveDirection = Vector3.zero;
 
     public int playerControlSpeed = 10;
-    public bool playerControlFlag;
+    public bool playerControlFlg;
     #endregion
 
     // Use this for initialization
     void Start() {
         Debug.Log("Start  ");
-        firstGPSflag = true;
-        playerControlFlag = true;
+        firstGPSFlg = true;
+        readDoneFlg = false;
+        playerControlFlg = true;
+        //streamCable.DataReceived += new SerialDataReceivedEventHandler(StreamCable_DataReceived);
+        streamCable.ReadTimeout = 100;
         streamCable.Open(); //Open the Serial Stream.
+    }
+
+    private void StreamCable_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+        if (readDoneFlg == true) {
+            // wait for main program done with the current buffer
+            return;
+        }
+        SerialPort sp = (SerialPort)sender;
+        char indata = (char)sp.ReadChar();
+        if (indata == '\n') {
+            readDoneFlg = true;
+        } else {
+            if (DEBUG) {
+                Debug.Log("read: " + indata);
+            }
+
+            jsonBuffer.Append(indata);
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        if (!playerControlFlag) {
-            readGPS();
-            move();
+        if (!playerControlFlg) {
+            if (readGPS()) {
+                move();
+            }
             if (Input.GetKeyDown(KeyCode.C)) {
-                playerControlFlag = true;
+                playerControlFlg = true;
                 LAT = 0;
                 LON = 0;
                 iniLAT = 0;
@@ -61,34 +89,58 @@ public class CameraControl : MonoBehaviour {
             // ccmove();
             playerControl();
             if (Input.GetKeyDown(KeyCode.C)) {
-                playerControlFlag = false;
+                playerControlFlg = false;
             }
         }
     }
 
-    void readGPS() {
-        if (streamCable.IsOpen) {
-            string jsonString = streamCable.ReadLine();
-            print(jsonString);
+    bool readGPS() {
+        if (!streamCable.IsOpen) {
+            Debug.Log("Serial port it not open!");
+        }
+        char indata;
+        while (true) {
             try {
-                JsonUtility.FromJsonOverwrite(jsonString, this);
-                Debug.Log("device = " + this.DEVICE);
-                Debug.Log("time_stamp = " + this.TIME_STAMP);
-                Debug.Log("status = " + this.STATUS);
-                Debug.Log("lat = " + this.LAT);
-                Debug.Log("lon = " + this.LON);
-                Debug.Log("alt = " + this.ALT);
-                Debug.Log("sat = " + this.SAT);
-                Debug.Log("prec = " + this.PREC);
-                Debug.Log("chars = " + this.CHARS);
-                Debug.Log("sentences = " + this.SENTENCES);
-                Debug.Log("csum_err = " + this.CSUM_ERR);
+                indata = (char)streamCable.ReadChar();
+                if (indata == '\n') {
+                    readDoneFlg = true;
+                    if (DEBUG) {
+                        Debug.Log("read: " + jsonBuffer);
+                    }
+                    break;
+                } else {
+
+                    jsonBuffer.Append(indata);
+                }
+            } catch (System.Exception) {
+            }
+        }
+        if (readDoneFlg) {
+
+            try {
+                JsonUtility.FromJsonOverwrite(jsonBuffer.ToString(), this);
+                jsonBuffer.Remove(0, jsonBuffer.Length);        // clear the buffer for the next reading
+                readDoneFlg = false;
+                if (DEBUG) {
+                    Debug.Log("device = " + this.DEVICE);
+                    Debug.Log("time_stamp = " + this.TIME_STAMP);
+                    Debug.Log("status = " + this.STATUS);
+                    Debug.Log("lat = " + this.LAT);
+                    Debug.Log("lon = " + this.LON);
+                    Debug.Log("alt = " + this.ALT);
+                    Debug.Log("sat = " + this.SAT);
+                    Debug.Log("prec = " + this.PREC);
+                }
+                return true;
             } catch (System.Exception) {
                 Debug.Log("fail to Json.");
+                jsonBuffer.Remove(0, jsonBuffer.Length);
+                readDoneFlg = false;
                 throw;
             }
         } else {
             Debug.Log("No  ");
+            return false;
         }
     }
 
@@ -128,11 +180,11 @@ public class CameraControl : MonoBehaviour {
     }
 
     void move() {
-        if (firstGPSflag == true && LON != 0 && LAT != 0 && ALT != 0) {
+        if (firstGPSFlg == true && LON != 0 && LAT != 0 && ALT != 0) {
             iniLON = LON;
             iniLAT = LAT;
             iniALT = ALT;
-            firstGPSflag = false;
+            firstGPSFlg = false;
         } else if (LON != 0 && LAT != 0) {
             float la = (float)((LAT - 53.178469) / 0.00001 * 0.12179047095976932582726898256213);
             float lo = (float)((LON - 53.178469) / 0.00001 * 0.12179047095976932582726898256213);
