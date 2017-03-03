4ef97ba5f98596e00cb4c09a7cb536e68cd0b106
khjtony <khjtony@gmail.com>
2017-03-02 16:39:20 -0800

    VR Control Kinda Done!

--- VRAR/Assets/Scripts/GenerateAndChangeLandscape.cs
+++ VRAR/Assets/Scripts/GenerateAndChangeLandscape.cs
@@ -17,6 +17,8 @@ public class Block {
 }
 
 public class GenerateAndChangeLandscape : MonoBehaviour {
+    // VR Camera
+    public Camera _CameraFacing;
     // x
     public static int _width = 150;
     // y
@@ -164,6 +166,9 @@ public class GenerateAndChangeLandscape : MonoBehaviour {
     }
     // Update is called once per frame
     void Update() {
+
+        #region Screen Version
+        /*
         if (Input.GetMouseButtonDown(0)) {
             // Debug.Log("Left Botton Detected");
             RaycastHit hit;
@@ -238,5 +243,111 @@ public class GenerateAndChangeLandscape : MonoBehaviour {
                 CheckObscuredNeighbours(blockPos);
             }
         }
+        */
+        #endregion
+
+        #region VR Version
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            // Ray ray = Camera.allCameras[0].ScreenPointToRay(new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0));
+            // Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0));
+            // Debug.Log(ray);
+            if (Physics.Raycast(new Ray(_CameraFacing.transform.position,
+                                     _CameraFacing.transform.rotation * Vector3.forward),
+                                     out hit, 5000.0f))
+            {
+                Vector3 blockPos = hit.transform.position;
+
+                // this is the bottom block. Don't delete it!!!!
+                if ((int)blockPos.y == 0) return;
+
+                worldBlocks[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = null;
+
+                Destroy(hit.transform.gameObject);
+
+                //
+                // Show hidden structure underneath
+                // Only instantiate hidden blocks when they are on demand!
+                //
+                for (int x = -1; x <= 1; x++)
+                {
+                    for (int y = -1; y <= 1; y++)
+                    {
+                        for (int z = -1; z <= 1; z++)
+                        {
+                            if (!(x == 0 && y == 0 && z == 0))
+                            {
+                                Vector3 neighbour = new Vector3(blockPos.x + x, blockPos.y + y, blockPos.z + z);
+                                DrawBlock(neighbour);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            // Debug.Log("Right Botton Detected");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0));
+            // Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+            // Debug.Log(ray.direction);
+            if (Physics.Raycast(new Ray(_CameraFacing.transform.position,
+                                     _CameraFacing.transform.rotation * Vector3.forward),
+                                     out hit, 5000.0f))
+            {
+                Vector3 blockPos = hit.transform.position;
+                Vector3 hitVector = blockPos - hit.point;
+
+                hitVector.x = Mathf.Abs(hitVector.x);
+                hitVector.y = Mathf.Abs(hitVector.y);
+                hitVector.z = Mathf.Abs(hitVector.z);
+
+                // Debug.Log(ray.direction);
+
+                if (hitVector.x > hitVector.z && hitVector.x > hitVector.y)
+                {
+                    // blockPos.x -= (int)Mathf.RoundToInt(ray.direction.x);
+                    if (ray.direction.x > 0)
+                    {
+                        blockPos.x -= 1;
+                    }
+                    else
+                    {
+                        blockPos.x += 1;
+                    }
+                }
+                else if (hitVector.y > hitVector.x && hitVector.y > hitVector.z)
+                {
+                    // blockPos.y -= (int)Mathf.RoundToInt(ray.direction.y);
+                    if (ray.direction.y > 0)
+                    {
+                        blockPos.y -= 1;
+                    }
+                    else
+                    {
+                        blockPos.y += 1;
+                    }
+                }
+                else
+                {
+                    // blockPos.z -= (int)Mathf.RoundToInt(ray.direction.z);
+                    if (ray.direction.z > 0)
+                    {
+                        blockPos.z -= 1;
+                    }
+                    else
+                    {
+                        blockPos.z += 1;
+                    }
+                }
+
+
+                CreateBlock((int)blockPos.y, blockPos, true);
+                CheckObscuredNeighbours(blockPos);
+            }
+        }
+        #endregion
     }
 }
